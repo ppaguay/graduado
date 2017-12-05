7c888f7ce69a8f4ece849e2dc32ae46a47d4329c
XL <XL@XL>
2017-12-05 16:59:51 -0500

    oferta laboral

---
+++ graduado/src/java/ec/edu/itsbolibar/www/bean/graduadosController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.itsbolibar.www.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author XL
+ */
+@ManagedBean
+@ViewScoped
+public class graduadosController {
+
+}
