c13dcc0d929e4956f0c2597539002a869364d769
Paul Paguay <ulvierps@hotmail.com>
2017-12-06 01:42:27 -0500

    tipoactividad

---
+++ graduado/src/java/ec/edu/itsbolibar/www/recursos/Util.java
@@ -0,0 +1,33 @@
+package ec.edu.itsbolibar.www.recursos;
+
+import java.util.Properties;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+public class Util {
+
+    public Util() {
+    }
+    
+    public static void addErrorMessage(  String msg) {
+        FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Transacción errónea", msg);
+        FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+    }
+
+    public static void addOKMessage( String msg) {
+        FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Transacción exitosa", msg);
+        FacesContext.getCurrentInstance().addMessage("successInfo", facesMsg);
+    }
+
+    public static void addErrorFatalMessage( String msg) {
+        FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_FATAL, "Transacción errónea", msg);
+        FacesContext.getCurrentInstance().addMessage("fatalInfo", facesMsg);
+    }
+
+    public static void addWarningMessage( String msg) {
+        FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Advertencia", msg);
+        FacesContext.getCurrentInstance().addMessage("warningInfo", facesMsg);
+    }
+}
