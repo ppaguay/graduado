97ac34dc085e6b678a73349648f0af6d6c098126
XL <XL@XL>
2017-12-06 17:53:13 -0500

    graduados

---
+++ graduado/src/java/ec/edu/itsbolibar/www/bean/graduadoAplicarOferta.java
@@ -0,0 +1,249 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.itsbolibar.www.bean;
+
+import ec.edu.itsbolivar.www.rnegocio.clases.Aplicar_oferta;
+import ec.edu.itsbolivar.www.rnegocio.clases.Graduado;
+import ec.edu.itsbolivar.www.rnegocio.clases.Oferta_laboral;
+import ec.edu.itsbolivar.www.rnegocio.funciones.FAplicar_oferta;
+import ec.edu.itsbolivar.www.rnegocio.funciones.FGraduado;
+import ec.edu.itsbolivar.www.rnegocio.funciones.FOferta_laboral;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author XL
+ */
+@ManagedBean
+@ViewScoped
+public class graduadoAplicarOferta {
+
+    Graduado graduado = new Graduado();
+    Aplicar_oferta item = new Aplicar_oferta();
+
+    ArrayList<Aplicar_oferta> lst_aplica_oferta = new ArrayList<>();
+
+    ArrayList<Oferta_laboral> lst_oferta_laboral = new ArrayList<>();
+
+    String fecha_aplica = "";
+    String fecha_inicio = "";
+
+    boolean modal = false;
+    boolean edit = false;
+
+    public graduadoAplicarOferta() {
+        try {
+            cargarDatos();
+        } catch (Exception ex) {
+            Logger.getLogger(historiaLaboralController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void activeNew() {
+        item = new Aplicar_oferta();
+        edit = false;
+    }
+
+    public void insertar() throws Exception {
+        item.setGraduado(graduado);
+        item.setFecha_inicio(convertStringToDateLong(fecha_inicio));
+        item.setFecha_aplica(convertStringToDateLong(fecha_aplica));
+        FAplicar_oferta.insertar(item);
+
+        modal = true;
+        cargarDatos();
+    }
+
+    public void actualizar() throws Exception {
+        modal = true;
+        item.setGraduado(graduado);
+        item.setFecha_inicio(convertStringToDateLong(fecha_inicio));
+        item.setFecha_aplica(convertStringToDateLong(fecha_aplica));
+        System.out.println("Modificando" + item.getGraduado().getNombre() + item.getOferta_laboral().getEmpresa().getNombre());
+        FAplicar_oferta.modificar(item);
+    }
+
+    public void eliminar(Aplicar_oferta e) throws Exception {
+        System.err.println("eliminado");
+
+        FAplicar_oferta.eliminar(e);
+        cargarDatos();
+    }
+
+    public void ver(Aplicar_oferta e) throws Exception {
+        edit = true;
+        item = FAplicar_oferta.obtener(e.getGraduado().getCodigo(), e.getOferta_laboral().getCodigo());
+        fecha_inicio = convertMillisecondsToString(item.getFecha_inicio());
+        fecha_aplica = convertMillisecondsToString(item.getFecha_aplica());
+//para que se carge los datos en los select
+
+        for (Oferta_laboral ta : lst_oferta_laboral) {
+            if (ta.getCodigo() == item.getOferta_laboral().getCodigo()) {
+                item.setOferta_laboral(ta);
+            }
+        }
+
+    }
+
+    private void cargarDatos() throws Exception {
+
+        graduado = itemLogueado();
+        lst_aplica_oferta.clear();
+        lst_aplica_oferta = FAplicar_oferta.obtener(graduado);
+
+        lst_oferta_laboral.clear();
+        lst_oferta_laboral = OfertaNoAplicadas();
+
+        fecha_aplica = optenerfechaActual();
+        fecha_inicio = optenerfechaActual();
+        System.out.println("" + fecha_inicio);
+
+    }
+
+    public ArrayList<Oferta_laboral> OfertaNoAplicadas() throws Exception {
+        ArrayList<Oferta_laboral> lst = new ArrayList<>();
+        ArrayList<Oferta_laboral> lst_return = new ArrayList<>();
+        lst = FOferta_laboral.obtener();
+
+        for (Oferta_laboral item : lst) {
+            boolean existe = false;
+            for (Aplicar_oferta a : lst_aplica_oferta) {
+                if (a.getOferta_laboral().getCodigo() == item.getCodigo()) {
+                    existe = true;
+                    System.out.println("ao:"+a.getOferta_laboral().getCodigo()+"o:"+item.getCodigo());
+                }
+
+            }
+            if (existe == false) {
+                lst_return.add(item);
+            }
+
+        }
+        return lst_return;
+    }
+
+    public Graduado itemLogueado() throws IOException {
+        Graduado item = (Graduado) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("graduado");
+
+        if (item != null) {
+
+        } else {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("login");
+        }
+        return item;
+
+    }
+//fin S
+
+    public String optenerfechaActual() throws ParseException {
+        LocalDate localDate = LocalDate.now();
+        return DateTimeFormatter.ofPattern("yyy-MM-dd").format(localDate).toString();
+
+    }
+
+    public String convertDateToString(Date fecha) {
+
+        Format formatter = new SimpleDateFormat("yyyy-mm-dd");
+        String s = formatter.format(fecha);
+        return s;
+
+    }
+
+    private Long convertStringToDateLong(String fecha) throws ParseException {
+
+        DateFormat format = new SimpleDateFormat("yyy-MM-dd");
+        Date date = format.parse(fecha);
+        return date.getTime();
+    }
+
+    public String convertMillisecondsToString(long fecha) throws ParseException {
+        String dateFormat = "yyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(fecha);
+        return simpleDateFormat.format(calendar.getTime());
+    }
+
+    //otros
+    public Aplicar_oferta getItem() {
+        return item;
+    }
+
+    public void setItem(Aplicar_oferta item) {
+        this.item = item;
+    }
+
+    public ArrayList<Aplicar_oferta> getLst_aplica_oferta() {
+        return lst_aplica_oferta;
+    }
+
+    public void setLst_aplica_oferta(ArrayList<Aplicar_oferta> lst_aplica_oferta) {
+        this.lst_aplica_oferta = lst_aplica_oferta;
+    }
+
+    public ArrayList<Oferta_laboral> getLst_oferta_laboral() {
+        return lst_oferta_laboral;
+    }
+
+    public void setLst_oferta_laboral(ArrayList<Oferta_laboral> lst_oferta_laboral) {
+        this.lst_oferta_laboral = lst_oferta_laboral;
+    }
+
+    public String getFecha_aplica() {
+        return fecha_aplica;
+    }
+
+    public void setFecha_aplica(String fecha_aplica) {
+        this.fecha_aplica = fecha_aplica;
+    }
+
+    public String getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public void setFecha_inicio(String fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public boolean isModal() {
+        return modal;
+    }
+
+    public void setModal(boolean modal) {
+        this.modal = modal;
+    }
+
+    public boolean isEdit() {
+        return edit;
+    }
+
+    public void setEdit(boolean edit) {
+        this.edit = edit;
+    }
+
+    public Graduado getGraduado() {
+        return graduado;
+    }
+
+    public void setGraduado(Graduado graduado) {
+        this.graduado = graduado;
+    }
+
+}
--- graduado/src/java/ec/edu/itsbolivar/www/rnegocio/funciones/FAplicar_oferta.java
+++ graduado/src/java/ec/edu/itsbolivar/www/rnegocio/funciones/FAplicar_oferta.java
@@ -1,146 +1,148 @@
 package ec.edu.itsbolivar.www.rnegocio.funciones;
+
 import ec.edu.itsbolivar.www.accesodatos.*;
 import ec.edu.itsbolivar.www.rnegocio.clases.Aplicar_oferta;
+import ec.edu.itsbolivar.www.rnegocio.clases.Graduado;
 import java.util.ArrayList;
-public class FAplicar_oferta{
 
+public class FAplicar_oferta {
 
- public static boolean insertar(Aplicar_oferta obj ) throws  Exception { 
- boolean band= false;
- String sql = "insert into aplicar_oferta values (?,?,?,?,?)";
-ArrayList<Parametro> lstpar= new ArrayList<Parametro>();
-
-//campos con referencias
-
-
-lstpar.add(new Parametro(1,obj.getGraduado().getCodigo()));
-
-lstpar.add(new Parametro(2,obj.getOferta_laboral().getCodigo()));
-
-//campos sin referencias
-
- lstpar.add(new Parametro(3,obj.getFecha_aplica()));
- lstpar.add(new Parametro(4,obj.getContrato()));
- lstpar.add(new Parametro(5,obj.getFecha_inicio()));
-try { 
-   band = AccesoDatos.ejecutaComando1(sql, lstpar);
-} catch (Exception ex) { 
-throw ex;}
- return band; 
-}
-
-
-
- public static boolean modificar(Aplicar_oferta obj ) throws  Exception { 
- boolean band= false;
- String sql = "update aplicar_oferta set cod_graduado=?,cod_ofertalaboral=?,fecha_aplica=?,contrato=?,fecha_inicio=? where cod_graduado=? and cod_ofertalaboral=?  ";
-ArrayList<Parametro> lstpar= new ArrayList<Parametro>();
+    public static boolean insertar(Aplicar_oferta obj) throws Exception {
+        boolean band = false;
+        String sql = "insert into aplicar_oferta values (?,?,?,?,?)";
+        ArrayList<Parametro> lstpar = new ArrayList<Parametro>();
 
 //campos con referencias
+        lstpar.add(new Parametro(1, obj.getGraduado().getCodigo()));
 
-
-lstpar.add(new Parametro(1,obj.getGraduado().getCodigo()));
-
-lstpar.add(new Parametro(6,obj.getGraduado().getCodigo()));
-
-lstpar.add(new Parametro(2,obj.getOferta_laboral().getCodigo()));
-
-lstpar.add(new Parametro(7,obj.getOferta_laboral().getCodigo()));
+        lstpar.add(new Parametro(2, obj.getOferta_laboral().getCodigo()));
 
 //campos sin referencias
+        lstpar.add(new Parametro(3, obj.getFecha_aplica()));
+        lstpar.add(new Parametro(4, obj.getContrato()));
+        lstpar.add(new Parametro(5, obj.getFecha_inicio()));
+        try {
+            band = AccesoDatos.ejecutaComando1(sql, lstpar);
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return band;
+    }
 
- lstpar.add(new Parametro(3,obj.getFecha_aplica()));
- lstpar.add(new Parametro(4,obj.getContrato()));
- lstpar.add(new Parametro(5,obj.getFecha_inicio()));
-try { 
-   band = AccesoDatos.ejecutaComando1(sql, lstpar);
-} catch (Exception ex) { 
-throw ex;}
- return band; 
-}
-
-
-
- public static boolean eliminar(Aplicar_oferta obj ) throws  Exception { 
- boolean band= false;
- String sql = "delete from aplicar_oferta where cod_graduado=? and cod_ofertalaboral=? ";
-ArrayList<Parametro> lstpar= new ArrayList<Parametro>();
+    public static boolean modificar(Aplicar_oferta obj) throws Exception {
+        boolean band = false;
+        String sql = "update aplicar_oferta set cod_graduado=?,cod_ofertalaboral=?,fecha_aplica=?,contrato=?,fecha_inicio=? where cod_graduado=? and cod_ofertalaboral=?  ";
+        ArrayList<Parametro> lstpar = new ArrayList<Parametro>();
 
 //campos con referencias
+        lstpar.add(new Parametro(1, obj.getGraduado().getCodigo()));
 
+        lstpar.add(new Parametro(6, obj.getGraduado().getCodigo()));
 
-lstpar.add(new Parametro(1,obj.getGraduado().getCodigo()));
+        lstpar.add(new Parametro(2, obj.getOferta_laboral().getCodigo()));
 
-lstpar.add(new Parametro(2,obj.getOferta_laboral().getCodigo()));
+        lstpar.add(new Parametro(7, obj.getOferta_laboral().getCodigo()));
 
 //campos sin referencias
+        lstpar.add(new Parametro(3, obj.getFecha_aplica()));
+        lstpar.add(new Parametro(4, obj.getContrato()));
+        lstpar.add(new Parametro(5, obj.getFecha_inicio()));
+        try {
+            band = AccesoDatos.ejecutaComando1(sql, lstpar);
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return band;
+    }
 
-try { 
-   band = AccesoDatos.ejecutaComando1(sql, lstpar);
-} catch (Exception ex) { 
-throw ex;}
- return band; 
-}
-
-
-
- public static Aplicar_oferta obtener (int pcod_graduado,int pcod_ofertalaboral) throws Exception  {  
- Aplicar_oferta miAplicar_oferta = null;
-try{ 
- String sql = "select cod_graduado,cod_ofertalaboral,fecha_aplica,contrato,fecha_inicio from aplicar_oferta where   cod_graduado=? and cod_ofertalaboral=? ";
-ArrayList<Parametro> lstpar = new ArrayList<Parametro>();
- lstpar.add(new Parametro(1,pcod_graduado));
- lstpar.add(new Parametro(2,pcod_ofertalaboral));
-ConjuntoResultado rs= AccesoDatos.ejecutaQuery(sql,lstpar);
-ArrayList<Aplicar_oferta> lst=llenarAplicar_ofertas(rs);
- for (Aplicar_oferta c : lst){
- miAplicar_oferta= c;
- } 
-
-} catch (Exception ex) { 
-throw ex; }
-return  miAplicar_oferta;
-}
-
-
- public static ArrayList<Aplicar_oferta> obtener () throws Exception  {  
- ArrayList<Aplicar_oferta> lst=new ArrayList<>();
-try{ 
- String sql = "select cod_graduado,cod_ofertalaboral,fecha_aplica,contrato,fecha_inicio from aplicar_oferta; ";
-ConjuntoResultado rs= AccesoDatos.ejecutaQuery(sql);
- lst=llenarAplicar_ofertas(rs);
-
-} catch (Exception ex) { 
-throw ex; }
-return  lst;
-}
-
-
- private static ArrayList<Aplicar_oferta> llenarAplicar_ofertas(ConjuntoResultado cr)  throws  Exception { 
- ArrayList<Aplicar_oferta> lst = new ArrayList<Aplicar_oferta>();
- Aplicar_oferta obj=null;
-try { 
-  while(cr.next()){
- obj = new Aplicar_oferta();
+    public static boolean eliminar(Aplicar_oferta obj) throws Exception {
+        boolean band = false;
+        String sql = "delete from aplicar_oferta where cod_graduado=? and cod_ofertalaboral=? ";
+        ArrayList<Parametro> lstpar = new ArrayList<Parametro>();
 
 //campos con referencias
+        lstpar.add(new Parametro(1, obj.getGraduado().getCodigo()));
 
-
-obj.setGraduado(FGraduado.obtener(cr.getInt(1)));
-
-obj.setOferta_laboral(FOferta_laboral.obtener(cr.getInt(2)));
+        lstpar.add(new Parametro(2, obj.getOferta_laboral().getCodigo()));
 
 //campos sin referencias
+        try {
+            band = AccesoDatos.ejecutaComando1(sql, lstpar);
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return band;
+    }
 
-obj.setFecha_aplica(cr.getLong(3));
-obj.setContrato(cr.getString(4));
-obj.setFecha_inicio(cr.getLong(5));
-lst.add(obj);
+    public static Aplicar_oferta obtener(int pcod_graduado, int pcod_ofertalaboral) throws Exception {
+        Aplicar_oferta miAplicar_oferta = null;
+        try {
+            String sql = "select cod_graduado,cod_ofertalaboral,fecha_aplica,contrato,fecha_inicio from aplicar_oferta where   cod_graduado=? and cod_ofertalaboral=? ";
+            ArrayList<Parametro> lstpar = new ArrayList<Parametro>();
+            lstpar.add(new Parametro(1, pcod_graduado));
+            lstpar.add(new Parametro(2, pcod_ofertalaboral));
+            ConjuntoResultado rs = AccesoDatos.ejecutaQuery(sql, lstpar);
+            ArrayList<Aplicar_oferta> lst = llenarAplicar_ofertas(rs);
+            for (Aplicar_oferta c : lst) {
+                miAplicar_oferta = c;
+            }
 
- }
-} catch (Exception ex) { 
-throw ex;}
- return lst;}
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return miAplicar_oferta;
+    }
 
+    public static ArrayList<Aplicar_oferta> obtener() throws Exception {
+        ArrayList<Aplicar_oferta> lst = new ArrayList<>();
+        try {
+            String sql = "select cod_graduado,cod_ofertalaboral,fecha_aplica,contrato,fecha_inicio from aplicar_oferta; ";
+            ConjuntoResultado rs = AccesoDatos.ejecutaQuery(sql);
+            lst = llenarAplicar_ofertas(rs);
+
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return lst;
+    }
+    public static ArrayList<Aplicar_oferta> obtener(Graduado item) throws Exception {
+        ArrayList<Aplicar_oferta> lst = new ArrayList<>();
+        try {
+             ArrayList<Parametro> lstpar = new ArrayList<Parametro>();
+            lstpar.add(new Parametro(1, item.getCodigo()));
+            String sql = "select cod_graduado,cod_ofertalaboral,fecha_aplica,contrato,fecha_inicio from aplicar_oferta WHERE cod_graduado=? ORDER BY cod_ofertalaboral desc; ";
+            ConjuntoResultado rs = AccesoDatos.ejecutaQuery(sql,lstpar);
+            lst = llenarAplicar_ofertas(rs);
+
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return lst;
+    }
+
+    private static ArrayList<Aplicar_oferta> llenarAplicar_ofertas(ConjuntoResultado cr) throws Exception {
+        ArrayList<Aplicar_oferta> lst = new ArrayList<Aplicar_oferta>();
+        Aplicar_oferta obj = null;
+        try {
+            while (cr.next()) {
+                obj = new Aplicar_oferta();
+
+//campos con referencias
+                obj.setGraduado(FGraduado.obtener(cr.getInt(1)));
+
+                obj.setOferta_laboral(FOferta_laboral.obtener(cr.getInt(2)));
+
+//campos sin referencias
+                obj.setFecha_aplica(cr.getLong(3));
+                obj.setContrato(cr.getString(4));
+                obj.setFecha_inicio(cr.getLong(5));
+                lst.add(obj);
+
+            }
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return lst;
+    }
 
 }
