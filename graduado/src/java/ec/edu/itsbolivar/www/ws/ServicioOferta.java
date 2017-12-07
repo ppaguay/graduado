87920b6a22cef8c56d2887aa75fa85c27f65286b
XL <XL@XL>
2017-12-07 07:56:44 -0500

    graduados

--- graduado/src/java/ec/edu/itsbolivar/www/ws/ServicioOferta.java
+++ graduado/src/java/ec/edu/itsbolivar/www/ws/ServicioOferta.java
@@ -5,6 +5,7 @@
  */
 package ec.edu.itsbolivar.www.ws;
 
+import com.google.gson.Gson;
 import ec.edu.itsbolivar.www.rnegocio.clases.Oferta_laboral;
 import ec.edu.itsbolivar.www.rnegocio.funciones.FOferta_laboral;
 import java.util.ArrayList;
@@ -26,7 +27,7 @@ public class ServicioOferta {
     @Path("/listar")
     @Produces({MediaType.APPLICATION_JSON})
     public List<Oferta_laboral> listar() {
-        List<Oferta_laboral> lista = new ArrayList<>();       
+        List<Oferta_laboral> lista = new ArrayList<>();
         try {
             lista = FOferta_laboral.obtener();
         } catch (Exception e) {
@@ -36,15 +37,45 @@ public class ServicioOferta {
     }
 
     @GET
+    @Path("/listar2")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String lista() {
+        ArrayList<Oferta_laboral> lista = new ArrayList<>();
+        try {
+            lista = FOferta_laboral.obtener();
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+
+        String json = new Gson().toJson(lista);
+        return json;
+    }
+
+    @GET
     @Path("/obtener/{codigo}")
     @Produces({MediaType.APPLICATION_JSON})
     public Oferta_laboral obtener(@PathParam("codigo") int codigo) {
-        Oferta_laboral oferta = null;        
+        Oferta_laboral oferta = null;
         try {
             oferta = FOferta_laboral.obtener(codigo);
         } catch (Exception e) {
             System.out.println("Error:" + e.getMessage());
         }
         return oferta;
+    }
+
+    @GET
+    @Path("/obtener2/{codigo}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String obtener2(@PathParam("codigo") int codigo) {
+        Oferta_laboral oferta = null;
+        try {
+            oferta = FOferta_laboral.obtener(codigo);
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+        String json = new Gson().toJson(oferta);
+        return "{"+json+"}";
+
     }
 }
